74d3e5ab74e0bd8f921c046c8082b1c3f0430774
Nataniel Carvalho <natanielmendes@hotmail.com>
2021-02-15 14:28:29 -0300

    CORS configuration to support API calls coming from React app

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
@@ -22,10 +23,11 @@ const eventRoutes = require('../src/routes/event');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(eventRoutes);
 
-const port = 3000;
+const port = 3001;
 app.listen(process.env.PORT || port, () => {
     let usedPort = process.env.PORT === undefined ? port : process.env.PORT;
     console.log('Server listening on ' + usedPort + ' port!');
