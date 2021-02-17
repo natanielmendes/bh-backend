05cc0c1ee4f168b14f2484491620e93386f04943
Nataniel Carvalho <natanielmendes@hotmail.com>
2021-02-17 16:54:58 -0300

    Setting eventDate as required in mongodb

--- src/models/event.js
+++ src/models/event.js
@@ -7,7 +7,7 @@ const eventSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
-    eventDate: { type: Date, required: false }
+    eventDate: { type: Date, required: true }
 });
 
 module.exports = mongoose.model('event', eventSchema);
